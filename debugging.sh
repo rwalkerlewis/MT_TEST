1ec369f0d21c57a0391b2d18f882f1b561344edc
Robert Walker <rainbowseismic@gmail.com>
2025-06-06 15:44:22 -0600

    added step05

--- debugging.sh
+++ debugging.sh
@@ -9,3 +9,23 @@ docker exec -i -t pylith-dev-workspace /bin/bash
 ps -uax
 
 gdb -p $PID
+
+# Reset ptrace
+
+# Run docker image in privileged mode as root.
+docker run -ti --privileged --rm -u root ghcr.io/geodynamics/pylith_installer/pylith-devenv /bin/bash
+
+# Verify ptrace setting needs updating
+cat /proc/sys/kernel/yama/ptrace_scope
+# If output is 1, then continue, if 0 then no need to change anything.
+
+# Verify ptrace setting is correct.
+cat /etc/sysctl.d/10-ptrace.conf
+# Output should be 0
+
+# Restart the procps service.
+service procps restart
+
+# Verify ptrace setting has changed
+cat /proc/sys/kernel/yama/ptrace_scope
+# Output should be 0
